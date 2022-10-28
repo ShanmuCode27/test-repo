27edd499158fa34fd3238c00f731f00fc1ed3be0
ShanmuCode27 <90304223+ShanmuCode27@users.noreply.github.com>
2022-10-28 10:45:26 +0530

    dafa

--- auth.js
+++ auth.js
@@ -63,85 +63,12 @@ const configureClient = async () => {
     domain: config.domain,
     clientId: config.clientId
   });
+
+  console.log(auth0Client);
 };
 
 
 window.onload = async () => {
   await configureClient();
 
-  // NEW - update the UI state
-  updateUI();
-};
-
-
-const login = async () => {
-  await auth0Client.loginWithRedirect({
-    authorizationParams: {
-      redirect_uri: window.location.origin
-    }
-  });
-};
-
-// ..
-
-window.onload = async () => {
-
-  // .. code ommited for brevity
-
-  updateUI();
-
-  const isAuthenticated = await auth0Client.isAuthenticated();
-  console.log(auth0Client);
-  console.log(isAuthenticated);
-
-  if (isAuthenticated) {
-    // show the gated content
-    return;
-  }
-
-  // NEW - check for the code and state parameters
-  const query = window.location.search;
-  if (query.includes("code=") && query.includes("state=")) {
-
-    // Process the login state
-    await auth0Client.handleRedirectCallback();
-    
-    updateUI();
-
-    // Use replaceState to redirect the user away and remove the querystring parameters
-    window.history.replaceState({}, document.title, "/");
-  }
-};
-
-// ..
-
-const logout = () => {
-  auth0Client.logout({
-    logoutParams: {
-      returnTo: window.location.origin
-    }
-  });
-};
-
-const updateUI = async () => { 
-  const isAuthenticated = await auth0Client.isAuthenticated();
-
-  document.getElementById("btn-logout").disabled = !isAuthenticated;
-  document.getElementById("btn-login").disabled = isAuthenticated;
-  
-  // NEW - add logic to show/hide gated content after authentication
-  if (isAuthenticated) {
-    document.getElementById("gated-content").classList.remove("hidden");
-
-    document.getElementById(
-      "ipt-access-token"
-    ).innerHTML = await auth0Client.getTokenSilently();
-
-    document.getElementById("ipt-user-profile").textContent = JSON.stringify(
-      await auth0Client.getUser()
-    );
-
-  } else {
-    document.getElementById("gated-content").classList.add("hidden");
-  }
 };
